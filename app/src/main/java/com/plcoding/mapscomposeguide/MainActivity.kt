d6e2aeb509785813bf5fa9ed57b54ff2e2abcb33
Nikita Aniperko <nikan0ne@mail.ru>
2023-10-23 19:43:03 +0300

    Final changes

--- app/src/main/java/com/plcoding/mapscomposeguide/MainActivity.kt
+++ app/src/main/java/com/plcoding/mapscomposeguide/MainActivity.kt
@@ -12,7 +12,9 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.plcoding.mapscomposeguide.presentation.MapScreen
 import com.plcoding.mapscomposeguide.ui.theme.MapsComposeGuideTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
--- app/src/main/java/com/plcoding/mapscomposeguide/presentation/MapsViewModel.kt
+++ app/src/main/java/com/plcoding/mapscomposeguide/presentation/MapsViewModel.kt
@@ -4,10 +4,32 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.google.android.gms.maps.model.MapStyleOptions
+import com.plcoding.mapscomposeguide.domain.model.ParkingSpot
+import com.plcoding.mapscomposeguide.domain.repository.ParkingSpotRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
-class MapsViewModel:ViewModel() {
+
+@HiltViewModel
+class MapsViewModel @Inject constructor(
+    private val repository: ParkingSpotRepository
+):ViewModel() {
     var state by  mutableStateOf(MapState())
+
+    init {
+        viewModelScope.launch {
+            repository.getParkingSpots().collectLatest {
+                spots->
+                state = state.copy(
+                    parkingSpots = spots
+                )
+            }
+        }
+    }
 
     fun onEvent(event: MapEvent){
         when (event){
@@ -21,6 +43,20 @@ class MapsViewModel:ViewModel() {
                     isFalloutMap = !state.isFalloutMap
                     )
             }
+            is MapEvent.OnMapLongClick->{
+                viewModelScope.launch {
+                    repository.insertParkingSpot(
+                        ParkingSpot(event.latLng.latitude,
+                            event.latLng.longitude)
+                    )
+                }
+            }
+            is MapEvent.OnInfoWindowLongClick->{
+                viewModelScope.launch {
+                    repository.deleteParkingSpot(event.spot)
+                }
+            }
+
         }
     }
 }
