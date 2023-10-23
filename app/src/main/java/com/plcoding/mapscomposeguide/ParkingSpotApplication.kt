3c060f6975a2d495e31d17a27fb15809d70a5a8f
Nikita Aniperko <nikan0ne@mail.ru>
2023-10-23 19:42:11 +0300

    ParkingSpotApplication and AppModule has been created

---
+++ app/src/main/java/com/plcoding/mapscomposeguide/ParkingSpotApplication.kt
@@ -0,0 +1,9 @@
+package com.plcoding.mapscomposeguide
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+
+@HiltAndroidApp
+class ParkingSpotApplication:Application() {
+}
